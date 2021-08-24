03d1125acc32f96ad3dc5f10b46f6fa5c84f9955
Shubham Singh <shubham88foru@gmail.com>
2021-08-24 11:26:51 +0530

    seperate single plugin into mutipleplugins and add workaround for css files

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,57 @@
+import * as esbuild from "esbuild-wasm";
+
+import axios from "axios";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "fileCache",
+});
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: "fetch-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: inputCode,
+          };
+        }
+
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) return cachedResult;
+
+        const { data, request } = await axios.get(args.path);
+
+        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents =
+          fileType === "css"
+            ? `
+         const style = document.createElement('style');
+         style.innerText = '${escaped}';
+         document.head.appendChild(style);
+        `
+            : data;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+    },
+  };
+};
