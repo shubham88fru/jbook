267c42926e95ce16cd71caf3f2cad5838407853f
Shubham Singh <shubham88fru@yahoo.com>
2021-08-26 11:29:59 +0530

    Finalize iframe changes setup

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
   const ref = useRef<any>();
+  const iframe = useRef<any>();
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -22,6 +22,9 @@ const App = () => {
 
   const onClick = async () => {
     if (!ref.current) return;
+
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -33,8 +36,28 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) =>  {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</did>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -45,7 +68,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit!</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        ref={iframe}
+        title="jbook-sandbox"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
