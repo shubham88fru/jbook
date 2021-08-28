9d8b4c12bb5581ecc6fbcb3733698b760b788f7f
Shubham Singh <shubham88fru@yahoo.com>
2021-08-28 19:46:55 +0530

    Code cleanup and organize project structure

---
+++ src/components/preview.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useRef } from "react";
+
+interface PreviewProps {
+  code: string;
+}
+
+const html = `
+<html>
+  <head></head>
+  <body>
+    <div id="root"></div>
+    <script>
+      window.addEventListener('message', (event) =>  {
+        try {
+          eval(event.data);
+        } catch (err) {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</did>';
+          console.error(err);
+        }
+      }, false);
+    </script>
+  </body>
+</html>
+`;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>();
+
+  useEffect(() => {
+    iframe.current.srcdoc = html;
+    iframe.current.contentWindow.postMessage(code, "*");
+  }, [code]);
+
+  return (
+    <iframe
+      ref={iframe}
+      title="jbook-sandbox"
+      sandbox="allow-scripts"
+      srcDoc={html}
+    />
+  );
+};
+
+export default Preview;
