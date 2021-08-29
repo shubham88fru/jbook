9fbe7b8e71160cf5e6abbe7ea8a1bb66e7970f0f
Shubham Singh <shubham88fru@yahoo.com>
2021-08-30 00:49:29 +0530

    Setup inbrowser markdown editor

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,44 @@
+import MDEditor from "@uiw/react-md-editor";
+import { useState, useEffect, useRef } from "react";
+import "./text-editor.css";
+
+const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
+      setEditing(false);
+    };
+
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-editor" onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
+    </div>
+  );
+};
+
+export default TextEditor;
