b3afa6364525c8ba6b54eda4a852a1354599fcd6
Shubham Singh <shubham88fru@yahoo.com>
2021-09-01 11:19:06 +0530

    Wireup react and redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,15 @@ import CodeCell from "./components/code-cell";
 import { Provider } from "react-redux";
 import { store } from "./state";
 import TextEditor from "./components/text-editor";
+import CellList from "./components/cell-list";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        {/* <TextEditor /> */}
         {/* <CodeCell /> */}
+        <CellList />
       </div>
     </Provider>
   );
