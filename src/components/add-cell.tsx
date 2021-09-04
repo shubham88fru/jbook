d60c9d91485891da063784be52d60869657b8014
Shubham Singh <shubham88fru@yahoo.com>
2021-09-04 11:02:38 +0530

    Finishing up react redux integration

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -2,18 +2,18 @@ import "./add-cell.css";
 import { useActions } from "../hooks/use-actions";
 
 interface AddCellProps {
-  nextCellId: string | null;
+  previousCellId: string | null;
   forceVisible?: boolean;
 }
 
-const AddCell: React.FC<AddCellProps> = ({ forceVisible, nextCellId }) => {
-  const { insertCellBefore } = useActions();
+const AddCell: React.FC<AddCellProps> = ({ forceVisible, previousCellId }) => {
+  const { insertCellAfter } = useActions();
   return (
     <div className={`add-cell ${forceVisible && "force-visible"}`}>
       <div className="add-buttons">
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, "code")}
+          onClick={() => insertCellAfter(previousCellId, "code")}
         >
           <span className="icon is-small">
             <i className="fas fa-plus"></i>
@@ -22,7 +22,7 @@ const AddCell: React.FC<AddCellProps> = ({ forceVisible, nextCellId }) => {
         </button>
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, "text")}
+          onClick={() => insertCellAfter(previousCellId, "text")}
         >
           <span className="icon is-small">
             <i className="fas fa-plus"></i>
--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -39,7 +39,7 @@ const reducer = produce((state: CellsState = initalState, action: Action) => {
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
       return state;
-    case ActionType.INSERT_CELL_BEFORE:
+    case ActionType.INSERT_CELL_AFTER:
       const cell: Cell = {
         content: "",
         type: action.payload.type,
@@ -51,9 +51,9 @@ const reducer = produce((state: CellsState = initalState, action: Action) => {
         (id) => id === action.payload.id
       );
       if (foundIndex < 0) {
-        state.order.push(cell.id);
+        state.order.unshift(cell.id);
       } else {
-        state.order.splice(foundIndex, 0, cell.id);
+        state.order.splice(foundIndex + 1, 0, cell.id);
       }
       return state;
     default:
