b258ade59d43e0059d8175d3c92b862ef50a1a6e
Shubham Singh <shubham88fru@yahoo.com>
2021-09-04 21:44:38 +0530

    More stuff

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,13 +1,15 @@
 import { ActionType } from "../action-types";
+import { Dispatch } from "redux";
 import {
   UpdateCellAction,
   DeleteCellAction,
   MoveCellAction,
   InsertCellAfterAction,
   Direction,
+  Action,
 } from "../actions";
-
 import { CellTypes } from "../cell";
+import bundle from "../../bundler";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -47,5 +49,26 @@ export const insertCellAfter = (
       id,
       type: cellType,
     },
+  };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId,
+      },
+    });
+
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: result,
+      },
+    });
   };
 };
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,8 +1,10 @@
 import cellsReducer from "./cellsReducer";
+import bundlesReducer from "./bundlesReducers";
 import { combineReducers } from "redux";
 
 const reducers = combineReducers({
   cells: cellsReducer,
+  bundles: bundlesReducer,
 });
 
 export default reducers;
