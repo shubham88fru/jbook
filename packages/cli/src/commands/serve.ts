2174207f02345afc303ed286f12f1b6dc20da84b
Shubham Singh <shubham88fru@yahoo.com>
2021-09-12 10:54:03 +0530

    cli

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,12 +1,24 @@
 import path from "path";
-import { Command, program } from "commander";
+import { Command } from "commander";
 import { serve } from "local-api";
 
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      );
+    } catch (err: any) {
+      if (err.code === "EADDRINUSE") {
+        console.error("Port is in use. Try running on a diff port");
+      } else {
+        console.log("Error", err.message);
+      }
+      process.exit(1);
+    }
   });
