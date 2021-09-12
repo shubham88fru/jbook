859f46354ced77c43fcd4c1e1e9ad10e12b27168
Shubham Singh <shubham88fru@yahoo.com>
2021-09-12 11:12:57 +0530

    serve react build files via local api

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,16 +1,19 @@
 import express from "express";
+import path from "path";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
+  const packagePath = require.resolve("local-client/build/index.html");
 
-  app.use(
-    createProxyMiddleware({
-      target: "http://localhost:3000",
-      ws: true,
-      logLevel: "silent",
-    })
-  );
+  app.use(express.static(path.dirname(packagePath)));
+  // app.use(
+  //   createProxyMiddleware({
+  //     target: "http://localhost:3000",
+  //     ws: true,
+  //     logLevel: "silent",
+  //   })
+  // );
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
