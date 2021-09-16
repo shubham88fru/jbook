8402c223290dd92dbe5b92adb398988c2c8325de
Shubham Singh <shubham88fru@yahoo.com>
2021-09-16 09:49:44 +0530

    pre npm publish work

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const serveCommand = new Command()
   .command("serve [filename]")
-  .description("Open a file for editing")
+  .description("Open a file for editing!")
   .option("-p, --port <number>", "port to run server on", "4005")
   .action(async (filename = "notebook.js", options: { port: string }) => {
     try {
